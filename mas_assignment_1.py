ce58d50164c034c8cbb2b224748ea5438805c412
Olekmi <michonskialeksander@gmail.com>
2019-11-04 16:39:08 +0100

    upd

--- mas_assignment_1.py
+++ mas_assignment_1.py
@@ -5,10 +5,17 @@ number_of_voters = 10
 
 def gen_preference_matrix(number_of_preferences,number_of_voters):
     preference_matrix = np.arange(number_of_preferences)
-    preference_matrix = np.repeat(preference_matrix,number_of_voters,axis=1)
-    print(preference_matrix)
-    return preference_matrix
+    preference_matrix = np.repeat([preference_matrix],number_of_voters,axis=0)
+    print(preference_matrix.T)
+    return preference_matrix.T
 outcome = {}
 outcome['B'] = 10
 print(outcome['B'])
+
+def calculate_outcome(preference_matrix):
+    
+    for j in range(len(preference_matrix)):
+        for i in preference_matrix[j]:
+            
+
 gen_preference_matrix(number_of_preferences,number_of_voters)
