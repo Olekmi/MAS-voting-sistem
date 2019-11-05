ac77930c1aaec3ff873b0c98336f6846e5359e1d
Ismail Alaoui Abdellaoui <i.alaouiabdellaoui@student.maastrichtuniversity.nl>
2019-11-05 01:06:17 +0100

    outcome function working
    also added 2 generate preference matrix functions

--- mas_assignment_1.py
+++ mas_assignment_1.py
@@ -1,21 +1,40 @@
 import numpy as np
 
 number_of_preferences = 3
-number_of_voters = 10
+number_of_voters = 4
 
-def gen_preference_matrix(number_of_preferences,number_of_voters):
+min_number_preference = 0
+max_number_preference = 10
+
+def gen_random_preference_matrix(number_of_preferences,number_of_voters):
+    preference_matrix = np.random.randint(low=min_number_preference,high=max_number_preference,size=(number_of_preferences,number_of_voters) )
+    return preference_matrix.T
+
+def generate_fixed_pref_matrix(number_of_preferences,number_of_voters):
     preference_matrix = np.arange(number_of_preferences)
     preference_matrix = np.repeat([preference_matrix],number_of_voters,axis=0)
-    print(preference_matrix.T)
     return preference_matrix.T
-outcome = {}
-outcome['B'] = 10
-print(outcome['B'])
+    
 
 def calculate_outcome(preference_matrix):
-    
-    for j in range(len(preference_matrix)):
-        for i in preference_matrix[j]:
-            
+    outcome = {}
+    for j in range(preference_matrix.shape[0]):
+        temp = []
+        for i in range(preference_matrix.shape[1]):
+            temp.append(preference_matrix[j][i])
+        unique, counts = np.unique(temp, return_counts=True)
+        dict_uniques = dict(zip(unique, counts))
+        for key in dict_uniques:
+            dict_uniques[key] *=  (preference_matrix.shape[0] - 1 - j)
+        for key in dict_uniques:
+            if key in outcome:
+                outcome[key] += dict_uniques[key]
+            else:
+                outcome[key] = dict_uniques[key]
+    print(outcome)
 
-gen_preference_matrix(number_of_preferences,number_of_voters)
+
+preference_matrix = gen_random_preference_matrix(number_of_preferences,number_of_voters)
+print(preference_matrix)
+calculate_outcome(preference_matrix)
+
